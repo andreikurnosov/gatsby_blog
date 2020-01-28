0187480d92b8cbdc5f72a99900d50a0b8d66b5f1
nixdie <nixdie@yandex.ru>
2020-01-29 00:28:32 +0700

    styling with styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from 'styled-components';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: green;
+`
 
 export default ({ data }) => (
   <Layout>
@@ -13,8 +23,10 @@ export default ({ data }) => (
       {
         data.allMarkdownRemark.edges.map(({node}) => (
           <div key={node.id}>
-            <span>{ node.frontmatter.title  } - {node.frontmatter.date }</span>
-            <p>{ node.excerpt }</p>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>{ node.frontmatter.title  } - {node.frontmatter.date }</BlogTitle>
+              <p>{ node.excerpt }</p>
+            </BlogLink>
           </div>
         ))
       }
@@ -24,7 +36,7 @@ export default ({ data }) => (
 
 export const query = graphql`
   query{
-    allMarkdownRemark {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
       totalCount
       edges {
         node {
@@ -34,6 +46,9 @@ export const query = graphql`
             description
             title
             date
+          }
+          fields {
+            slug
           }
         }
       }
